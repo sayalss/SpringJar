de6f32db5f86deacc8687b9909ac3dd91fd77cbf
sayalss <sayali.sowale@gmail.com>
2024-09-26 08:29:42 -0400

    Spring jar project onboarding

---
+++ src/main/java/com/example/spring_jar/SpringJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_jar/SpringJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
